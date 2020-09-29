f2f1403f85b39b9bdaafea5cf379829f2c6323ca
Collin Holmquist <collinholmquist@gmail.com>
2020-09-29 15:06:43 -0500

    Create DeathBot.java

---
+++ DeathBot.java
@@ -0,0 +1,16 @@
+public class DeathBot {
+
+  public static void main(String [] args){
+    
+    pleasantBot(-10, 0, "W"); //output is SH
+    pleasantBot(10, 0, "N"); //output is TL TL TL SH
+    pleasantBot(3, 7, "W"); //output is TL TL MV MV MV TL SH
+    pleasantBot(-5, -3, "E"); //output is TL TL TL MV MV MV TL TL TL SH
+  
+  }
+  
+  public static void pleasantBot(int ew, int ns, String dir) {
+  
+  
+  }
+}
